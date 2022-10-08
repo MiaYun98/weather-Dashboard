699f472ab3c844bfb2c148e14a2478ef9d254b27
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-10-07 20:32:52 -0700

    render is done@@@ now I have to work on the buttongit add .

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,12 @@ var historyList = document.querySelector('#history');
 var inputEl = document.querySelector('.form-input');
 var forecastEl = document.querySelector('.forecast');
 var resultPage = document.querySelector(".result");
+var cityname = "";
 var saveEl = [];
-
-var cityname = ""
 
 // day information
 var currentTime = moment();
 $(".time").text(currentTime.format("MMM Do, YYYY"));
-
-
 
 // submitting the variable
 var submitFormEl = $("#search-form");
@@ -24,20 +21,16 @@ function gettingInfo (event) {
         alert("should write something");
         return;
     }
-    
-    // for(var i = 0; i < saveEl.length; i++) {
-    //     if (saveEl[i] !== searchName) {
-    //         console.log("repeated");
-    //         return;
-    //     } 
-    // }
+    //local storage setting array
+
     forecastEl.textContent = "";
     inputEl.value = "";
     // next function which is searching the data
-    cityname = searchName;
     // url goes to the java script with the fetch 
-    weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=416af2287105033badad2b026eeca30c"
+    weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchName + "&units=imperial&appid=416af2287105033badad2b026eeca30c"
+    todayUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchName + "&units=imperial&appid=416af2287105033badad2b026eeca30c"
     getWeatherInfo(weatherUrl);
+    gettodayweather(todayUrl);
 }
 
 // with the url getting the weather data
@@ -45,12 +38,8 @@ function getWeatherInfo(request) {
     fetch(request)
     .then(function (response) {
         if (response.ok) {
-            console.log(response);  
             response.json()
             .then(function (data) {
-                console.log(data.list);
-                history();
-                // todayweather(data);
                 fivedayweather(data);
             });
         } else {
@@ -62,49 +51,103 @@ function getWeatherInfo(request) {
     });
 }
 
-// function todayweather() {
-    
-//     var bigBox = $(".forecast")
-//     var day = document.createElement('ul');
-//     var list = document.createElement('li');
-//     day.appendChild(list);
-//     bigBox.appendChild(day);
-// }
+// getting current weather API fetching them in to readable data
+function gettodayweather(request) {
+    fetch(request)
+    .then(function (response) {
+        if (response.ok) {
+            response.json()
+            .then(function (data) {
+                todayweather(data);
+            });
+        } else {
+            alert('Error ' + response.statusText)
+        }   
+    })
+    .catch(function (error) {
+        alert('unable to connet to the Weather Data')
+    });
+}
+
+// current weather from the second API
+function todayweather(data) {
+    console.log(data);
+    var statename = document.querySelector(".search-current");
+    statename.textContent = data.name;
+    var img = document.createElement('img');
+    var currentIcon = "http://openweathermap.org/img/wn/"+ data.weather[0].icon +"@2x.png";
+    img.setAttribute('src', currentIcon);
+    img.setAttribute('alt', data.weather[0].description);
+    var currentInfo = document.querySelector(".current-info");
+    currentInfo.innerHTML =`temp: ${data.main.temp} °F` + "<br>"
+    currentInfo.innerHTML +=`wind: ${data.wind.speed} MPH` + "<br>"
+    currentInfo.innerHTML += `humidity: ${data.main.humidity} %`
+    var dataName = data.name;
+    listofName(dataName);
+    statename.appendChild(img);
+}
+
+// if there are redundent search history return 
+function listofName(name) {
+    for(var i = 0; i < saveEl.length; i++) {
+        if (saveEl[i] === name) {
+            return;
+        }
+    }
+    if (saveEl.length >= 8) {
+        saveEl.push(`${name}`);
+        saveEl.pop();
+    } else {
+        saveEl.push(`${name}`);  
+    }
+    //with the added thing render the history again
+    historyrender(name);
+}
 
 // println the data into the box for the five day weather information
 function fivedayweather(data) {
     resultPage.classList.remove("hidden");
-    for (var i = 0; i < 40; i+=8) {
+    for (var i = 5; i < data.list.length; i+=8) {
         var thread = document.createElement('thread');
         var tr = document.createElement('tr');
-        var th = document.createElement('th')
+        var th = document.createElement('th');
+        var td = document.createElement('td')
+        var img = document.createElement('img');
+        tr.textContent = data.list[i].dt_txt;
+        var iconLink = "http://openweathermap.org/img/wn/"+ data.list[i].weather[0].icon +"@2x.png";
+        img.setAttribute('src', iconLink);
+        img.setAttribute('alt', data.list[i].weather[0].main);
 
-        th.textContent = data.list[i].dt_txt + `\n
-        temp: ` + data.list[i].main.temp + `
-        weather: ` + data.list[i].weather[0].main + `
-        wind: ` + data.list[i].wind.speed + `
-        humidity: ` + data.list[i].main.humidity;
-
+        td.innerHTML =`temp: ${data.list[i].main.temp} °F` + "<br>"
+        td.innerHTML +=`wind: ${data.list[i].wind.speed} MPH` + "<br>"
+        td.innerHTML += `humidity: ${data.list[i].main.humidity} %`
+        
+        tr.appendChild(img);
+        tr.appendChild(td);
         tr.appendChild(th);
-        thread.appendChild(th);
-        console.log(th)
-        thread.setAttribute("class", "col-lg-2 col-12 five")
+        thread.appendChild(tr);
+        thread.setAttribute("class", "col-lg-2 col-12 five");
         forecastEl.appendChild(thread);
     }
-    
 }
 
+// local storage
 function local() {
     localStorage.setItem("saveEl",JSON.stringify(saveEl));  
 }
 
-function history() { 
-    //for loop to not repeat the same things on the list
-    var historyEl = document.createElement('li');
-    historyEl.setAttribute("id","button");
-    historyEl.textContent = cityname.toLowerCase();
-    historyList.appendChild(historyEl);
-    console.log(cityname);
+
+function historyrender(name) { 
+    // for loop to not repeat the same things on the list
+    console.log(name);
+    console.log(saveEl);
+    historyList.textContent = "";
+    for (var i = 0; i < saveEl.length; i++) {
+        var historyEl = document.createElement('li');
+        historyEl.setAttribute("id","button");
+        historyEl.textContent = saveEl[i];
+        historyList.appendChild(historyEl);
+    }
 }
 
 submitFormEl.on('submit', gettingInfo);
